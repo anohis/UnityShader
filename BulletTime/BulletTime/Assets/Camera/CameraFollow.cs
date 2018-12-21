217a184ece84229385bf9ed8ad0e5d0817071a2a
anohis <38350932+anohis@users.noreply.github.com>
2018-12-21 14:12:04 +0800

    .

--- BulletTime/BulletTime/Assets/Camera/CameraFollow.cs
+++ BulletTime/BulletTime/Assets/Camera/CameraFollow.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using ToolKit.Math;
 using UnityEngine;
 
 namespace Camera
@@ -32,21 +33,24 @@ namespace Camera
             }
             NewRotate.x = NewRotate.x % 360;
             NewRotate.y = NewRotate.y % 360;
-            if (NewRotate.x > RotateMax.x) NewRotate.x = RotateMax.x;
-            else if (NewRotate.x < RotateMin.x) NewRotate.x = RotateMin.x;
-        }
+            NewRotate.z = NewRotate.z % 360;
+
+			NewRotate.x = MathFunc.Saturate(NewRotate.x, RotateMin.x, RotateMax.x);
+			NewRotate.y = MathFunc.Saturate(NewRotate.y, RotateMin.y, RotateMax.y);
+			NewRotate.z = MathFunc.Saturate(NewRotate.z, RotateMin.z, RotateMax.z);
+		}
 
         void LateUpdate()
         {
             Vector3 targetPos = target.position + BasePos;
-            //避免過大的角度移動而造成畫面忽前忽後
-            CamRotate = Vector3.Lerp(CamRotate, NewRotate, Time.deltaTime * Speed);
-            Quaternion rotation = Quaternion.Euler(CamRotate);
+			CamRotate = NewRotate;
+			Quaternion rotation = Quaternion.Euler(CamRotate);
             CamPosition = Vector3.up * DistanceUp - Vector3.forward * DistanceAway;
             //四位數乘上向量表示轉向，也可以看成以原點旋轉
             CamPosition = rotation * CamPosition + targetPos;
-            transform.position = Vector3.Lerp(transform.position, CamPosition, Time.deltaTime * Speed);
-            transform.LookAt(targetPos);
-        }
+			//transform.position = Vector3.Lerp(transform.position, CamPosition, Time.deltaTime * Speed);
+			transform.position = CamPosition;
+			transform.rotation = Quaternion.Euler(CamRotate + Vector3.right * Mathf.Atan2(DistanceUp, DistanceAway) * 180 / Mathf.PI);
+		}
     }
 }
