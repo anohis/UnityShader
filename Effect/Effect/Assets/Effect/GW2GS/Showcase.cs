2bd6dab181332c7d20868c4003b524bb90c84943
anohis <38350932+anohis@users.noreply.github.com>
2019-06-27 23:19:03 +0800

    .

--- Effect/Effect/Assets/Effect/GW2GS/Showcase.cs
+++ Effect/Effect/Assets/Effect/GW2GS/Showcase.cs
@@ -15,15 +15,22 @@ namespace Effect.GS
         private readonly int _lightAttackHash = Animator.StringToHash("LightAttack");
         private readonly int _attackIndexHash = Animator.StringToHash("AttackIndex");
         private readonly int _hashStateTimeHash = Animator.StringToHash("StateTime");
+        private readonly int _forwardSpeedHash = Animator.StringToHash("ForwardSpeed");
 
         private bool _wantLightAttack;
 
         private float _comboTime = 0;
         private int _comboIndex = 0;
+        private bool _isRun;
 
         public void LightAttack()
         {
             _wantLightAttack = true;
+        }
+        public void Run()
+        {
+            _isRun = !_isRun;
+            _animator.SetFloat(_forwardSpeedHash, _isRun ? 10.0f : 0.0f);
         }
 
         private void Update()
@@ -33,7 +40,6 @@ namespace Effect.GS
             CheckComboTime(Time.deltaTime);
             CheckAttack();
         }
-
         private void CheckComboTime(float deltaTime)
         {
             _comboTime += deltaTime;
@@ -42,7 +48,6 @@ namespace Effect.GS
                 _animator.SetInteger(_attackIndexHash, 0);
             }
         }
-
         private void CheckAttack()
         {
             _animator.ResetTrigger(_attackHash);
